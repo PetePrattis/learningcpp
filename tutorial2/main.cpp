f5a9d86ba33a42f748dc016161007edede391fee
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-10-11 17:12:52 +0300

    Add files via upload

---
+++ tutorial2/main.cpp
@@ -0,0 +1,24 @@
+#include iostream
+
+using namespace std;
+
+void checkAge(int age)
+{
+
+   if (age18)
+   {
+       cout you are an adult;
+   }
+
+    if (age18)
+    {
+        cout  you are not an adult;
+    }
+}
+ int main ()
+{    int x;
+     cout write your age endl;
+     cin x;
+     checkAge(x);
+     return 0;
+ }
